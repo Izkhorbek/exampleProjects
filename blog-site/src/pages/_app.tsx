efd92ea8ee04631900743681f8be3607ecfba771
Izhorbek <salomuz@naver.com>
2024-10-28 23:05:13 +0900

    start next js(pages)

---
+++ blog-site/src/pages/_app.tsx
@@ -0,0 +1,7 @@
+import "src/styles/global-style.css";
+import { AppProps } from "next/app";
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default App;
---
+++ blog-site/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Page = () => {
+  return (
+    <>
+      <Head>
+        <title>Sammi Blog</title>
+      </Head>
+      <h1>Salom, Next.js!</h1>;
+    </>
+  );
+};
+
+export default Page;
